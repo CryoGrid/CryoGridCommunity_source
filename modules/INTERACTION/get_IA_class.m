59160508bdcb95889bfeb68eabe281d059d535be
nitinchj <45035518+nitinchj@users.noreply.github.com>
2019-11-19 11:47:25 +0100

    Merge

--- modules/INTERACTION/get_IA_class.m
+++ modules/INTERACTION/get_IA_class.m
@@ -1,10 +1,5 @@
 function ia_class = get_IA_class(class1, class2) %change this later, class1 and 2 are strings
- 
- 
-%ia_class = IA_HEAT_SALT;
- 
-%replace by matrix, this is stupid
- 
+
 if strcmp((class1), 'GROUND_freeW_seb') && strcmp((class2), 'GROUND_freeW_seb')
     ia_class = IA_HEAT();
 elseif strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'GROUND_freeW_seb_snow')
