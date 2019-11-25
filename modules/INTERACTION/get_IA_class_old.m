bdb1be3e2c006830bcbbad20321bf198e91bae25
robin-zweigel <57904608+robin-zweigel@users.noreply.github.com>
2019-11-25 09:19:38 +0100

    Original get_IA_class function

---
+++ modules/INTERACTION/get_IA_class_old.m
@@ -0,0 +1,46 @@
+function ia_class = get_IA_class(class1, class2) %change this later, class1 and 2 are strings
+
+
+%ia_class = IA_HEAT_SALT;
+
+%replace by matrix, this is stupid
+
+if strcmp((class1), 'GROUND_freeW_seb') && strcmp((class2), 'GROUND_freeW_seb')
+    ia_class = IA_HEAT();
+elseif strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'GROUND_freeW_seb_snow')
+    ia_class = IA_HEAT();
+elseif (strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'GROUND_freeW_seb')) || (strcmp((class2), 'GROUND_freeW_seb_snow') && strcmp((class1), 'GROUND_freeW_seb'))
+    ia_class = IA_HEAT();
+elseif (strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'SNOW_simple_seb_bucketW')) || (strcmp((class2), 'GROUND_freeW_seb_snow') && strcmp((class1), 'SNOW_simple_seb_bucketW'))
+    ia_class = IA_HEAT();
+elseif (strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'SNOW_simple_seb_crocus')) || (strcmp((class2), 'GROUND_freeW_seb_snow') && strcmp((class1), 'SNOW_simple_seb_crocus'))
+    ia_class = IA_HEAT();
+elseif strcmp((class1), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class2), 'SNOW_simple_seb_bucketW') || strcmp((class2), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class1), 'SNOW_simple_seb_bucketW')
+    ia_class = IA_HEAT();
+elseif strcmp((class1), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class2), 'SNOW_crocus_no_inheritance') || strcmp((class2), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class1), 'SNOW_crocus_no_inheritance')
+    ia_class = IA_HEAT();
+elseif strcmp((class1), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class2), 'GROUND_freeW_seb') || strcmp((class2), 'GROUND_fcSimple_salt_seb_snow') && strcmp((class1), 'GROUND_freeW_seb')
+    ia_class = IA_HEAT_SALT();
+elseif strcmp((class1), 'GROUND_fcSimple_salt_seb') && strcmp((class2), 'GROUND_freeW_seb') || strcmp((class2), 'GROUND_fcSimple_salt_seb') && strcmp((class1), 'GROUND_freeW_seb')
+    ia_class = IA_HEAT_SALT();
+elseif strcmp((class1), 'GROUND_fcSimple_salt_seb') && strcmp((class2), 'GROUND_freeW_seb_snow') || strcmp((class2), 'GROUND_fcSimple_salt_seb') && strcmp((class1), 'GROUND_freeW_seb_snow')
+    ia_class = IA_HEAT_SALT();
+elseif strcmp((class1), 'GROUND_freeW_bucketW_seb') && strcmp((class2), 'GROUND_freeW_seb')
+    ia_class = IA_HEAT_WATER_1();
+elseif strcmp((class1), 'GROUND_freeW_bucketW_seb_snow') && strcmp((class2), 'GROUND_freeW_seb')
+    ia_class = IA_HEAT_WATER_1();
+elseif strcmp((class1), 'SNOW_simple_seb_bucketW') && strcmp((class2), 'GROUND_freeW_bucketW_seb_snow')
+    ia_class = IA_HEAT_WATER_SNOW_GROUND();
+elseif strcmp((class1), 'SNOW_simple_seb_crocus') && strcmp((class2), 'GROUND_freeW_bucketW_seb_snow')
+    ia_class = IA_HEAT_WATER_SNOW_GROUND();
+elseif strcmp((class1), 'SNOW_crocus_no_inheritance') && strcmp((class2), 'GROUND_freeW_bucketW_seb_snow')
+    ia_class = IA_HEAT_WATER_SNOW_GROUND();
+elseif strcmp((class1), 'SNOW_crocus_no_inheritance') && strcmp((class2), 'GROUND_freeW_bucketW_seb_snow')
+    ia_class = IA_HEAT_WATER_SNOW_GROUND();
+elseif (strcmp((class1), 'GROUND_freeW_seb_snow') && strcmp((class2), 'SNOW_simple_seb')) || (strcmp((class2), 'GROUND_freeW_seb_snow') && strcmp((class1), 'SNOW_simple_seb'))
+    ia_class = IA_HEAT();
+else
+    ia_class=0;
+    disp('combination of classes not supported')
+end
+end
--- modules/IO/INITIALIZE_PARAMETER_FILE/read_lateral_from_file.m
+++ modules/IO/INITIALIZE_PARAMETER_FILE/read_lateral_from_file.m
@@ -15,8 +15,8 @@ function lateral = read_lateral_from_file(varargin)
             section = status_info(pos_list(i,1):pos_list(i,2),:);
             class_name=section{2,1};
             class_handle = str2func(section{2,1});
-            lateral=class_handle();
-            lateral=provide_variables(lateral);
+            lateral = class_handle();
+            lateral = provide_variables(lateral);
             lateral = initalize_from_file(lateral, section);
         end
     end
