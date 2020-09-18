2ac88e8fad7eee3eba062a5eb0830f23a87e3fee
thin <tin@byg.dtu.dk>
2020-09-18 09:38:22 +0200

    CHANGED: BASE_LATERAL and LAT_REMOVE_SURFACE_WATER and LAT_WATER_RESERVOIR classes updated to request parameters and constants from the PROVIDER classes. All previously hardcoded parameters have been removed.

--- modules/LATERAL/LAT_WATER_RESERVOIR.m
+++ modules/LATERAL/LAT_WATER_RESERVOIR.m
@@ -1,19 +1,22 @@
 
 classdef LAT_WATER_RESERVOIR < BASE_LATERAL
-
     
     methods
+        function self = LAT_WATER_RESERVOIR(index, pprovider, cprovider)  
+            self@BASE_LATERAL(index, pprovider, cprovider);
+        end
+        
         
         function lateral = provide_CONST(lateral)
-            lateral.CONST.day_sec = 24 .* 3600;
+            lateral.CONST.day_sec = [];
         end
         
         function lateral = provide_PARA(lateral)
-            lateral.PARA.reservoir_elevation = 20;
-            lateral.PARA.reservoir_temperature = 0.5; %only active for Xice classes - if empty, water added at the temperature of the respective grid cell
-            lateral.PARA.hardBottom_cutoff = 0.03; %hard bottom if saturated and water content below
-            lateral.PARA.distance_reservoir = 3; 
-            lateral.PARA.reservoir_contact_length = 1;
+            lateral.PARA.reservoir_elevation = [];
+            lateral.PARA.reservoir_temperature = []; %only active for Xice classes - if empty, water added at the temperature of the respective grid cell
+            lateral.PARA.hardBottom_cutoff = []; %hard bottom if saturated and water content below
+            lateral.PARA.distance_reservoir = []; 
+            lateral.PARA.reservoir_contact_length = [];
         end
         
         function lateral = provide_STATVAR(lateral)
@@ -28,7 +31,6 @@ classdef LAT_WATER_RESERVOIR < BASE_LATERAL
         end
         
         function lateral = push(lateral, forcing)
-            
             CURRENT = lateral.PARENT.TOP.NEXT;
             lateral.TEMP.open_system = 1; %start with open system
             while ~(strcmp(class(CURRENT), 'Bottom'))
@@ -36,7 +38,6 @@ classdef LAT_WATER_RESERVOIR < BASE_LATERAL
                 CURRENT = compute_diagnostic(CURRENT, forcing);
                 CURRENT = CURRENT.NEXT;
             end
-            
         end
         
         function lateral = set_ACTIVE(lateral, i, t)
