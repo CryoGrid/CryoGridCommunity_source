3e56abbea22656903ce3c3697cef28ac679ed053
thin <tin@byg.dtu.dk>
2021-06-05 21:50:44 -0200

    Added yaml_provider, not yet updated to align with new excel_provider

---
+++ source/IO/PROVIDER/PROVIDER_YAML.m
@@ -0,0 +1,163 @@
+classdef PROVIDER_YAML < BASE_PROVIDER
+
+    
+    methods
+        
+        function provider = assign_paths_yaml(provider, run_name, result_path, constant_file)
+            
+			warning(['WARNING: YAML provider not fully implemented and tested!']
+			
+            constant_file = [result_path run_name '/' constant_file '.yml'];
+            parameter_file = [result_path run_name '/' run_name '.yml'];
+
+            provider.PARA.run_name = run_name;
+            provider.PARA.result_path = result_path;
+            provider.PARA.parameter_file = parameter_file;
+            provider.PARA.constant_file = constant_file;
+            %provider.PARA.forcing_path = forcing_path;
+        end
+       
+        
+        function provider = read_const_yaml(provider)
+            
+            data = yaml.ReadYaml(provider.PARA.constant_file);
+            fnames = fieldnames(data.CONST);
+            for i=1:length(fnames)
+                if ~isnan(data.CONST.(fnames{i}))
+                    provider.CONST.(fnames{i}) = data.CONST.(fnames{i});
+                end
+            end
+        end
+        
+        function provider = read_parameters_yaml(provider)
+            
+            %read the parameter file
+            data = yaml.ReadYaml(provider.PARA.parameter_file);
+
+            % get all the CLASS sections defined in the file
+            class_sections = fieldnames(data); 
+            
+            % loop over all sections
+            for i = 1:length(class_sections)
+                section = data.(class_sections{i});
+                
+                % loop over definitions in section
+                for k = 1:length(section)
+                    % instantiate new class
+                    class_name = section{k}.name;
+                    class_index = section{k}.index;
+                    class_func = str2func(class_name);
+                    new_class = class_func();  %initialize the class
+                    
+                    %initialize CONST
+                    new_class = provide_CONST(new_class);
+                    if ~isempty(new_class.CONST)
+                        fieldnames_CONST  = fieldnames(new_class.CONST); %list of field in CONST
+                        for ii = 1:size(fieldnames_CONST,1)
+                            if isfield(provider.CONST, fieldnames_CONST{ii,1})
+                                new_class.CONST.(fieldnames_CONST{ii,1}) = provider.CONST.(fieldnames_CONST{ii,1});
+                            else
+                                warning(['WARNING: constant ' fieldnames_CONST{ii,1} ' in class ' class(new_class) ' not populated.'])
+                            end
+                        end
+                    end
+                    
+                    %provide STATVAR, will be initialized later
+                    new_class = provide_STATVAR(new_class);
+                    
+                    %initialize PARA
+                    new_class = provide_PARA(new_class);
+                    if ~isempty(new_class.PARA)
+                        fieldnames_PARA  = fieldnames(new_class.PARA); %list of field in PARA, find the field and populate it
+                        for ii = 1:size(fieldnames_PARA,1)
+                            fieldname = fieldnames_PARA{ii};
+                            if ~isfield(section{k}, fieldname)
+                                warning(['WARNING: parameter ' fieldname ' in class ' class(new_class) ' not populated.'])
+                                continue
+                            end
+                            
+                            contents = section{k}.(fieldname);
+                            
+                            if iscell(contents)   
+                                % it is a cell array
+                                % which means it is some sort of list or 1D array
+                                if ~isempty(contents) && isnumeric(contents{1})
+                                    % it is a number array
+                                    new_class.PARA.(fieldname) = cell2mat(contents)';
+                                else
+                                    % it is a cell array of strings
+                                    new_class.PARA.(fieldname) = contents';
+                                end
+                            elseif isstruct(contents)
+                                % it is a structure
+                                if ~isfield(contents,'type') || ~strcmp(contents.type,'V_MATRIX')
+                                    warning(['WARNING: datatype of ' fieldname ' was not recognized in file ' provider.PARA.parameter_file '.\n'
+                                             '         parameter ' fieldname ' in class ' class(new_class) ' not populated.']);
+                                    continue
+                                end
+                                    
+                                % so it is a V_MATRIX, a table with columnar data
+
+                                % If matrix contains any string values,
+                                % it is read as a 1D cell array of 1D cell arrays.
+                                if size(contents.values,1) == 1 && iscell(contents.values{1})                                
+                                    % this matrix contains some text data
+                                    % handle this special case
+                                    contents.values = vertcat(contents.values{:});
+                                    % now it has same format as numeric matrix
+                                end
+                                
+                                for kk = 1:length(contents.names)
+                                    cname = contents.names{kk};
+
+                                    % check type, just to be sure
+                                    if isnumeric(contents.values{1,kk})
+                                        % it is a numeric type, so convert
+                                        % to matrix
+                                        new_class.PARA.(fieldname).(cname) = cell2mat(contents.values(:,kk));
+                                    else
+                                        % it contains text, keep as cell array
+                                        new_class.PARA.(fieldname).(cname) = contents.values(:,kk);
+                                    end
+                                end
+                            elseif isempty(contents)
+                                new_class.PARA.(fieldname) = NaN;
+                            else
+                                % It is just a value or string, keep it
+                                new_class.PARA.(fieldname) = contents;
+                            end
+                        end
+                    end
+                    %mandatory function in all classes compatible with
+                    %provider_YAML
+                    warning('Calling "initialize_excel@new_class". Please reconsider need for this method!')
+                    new_class = initialize_excel(new_class);
+
+                    provider.CLASSES.(class_name){class_index,1} = new_class;
+                    
+                    if strcmp(class_sections{i}, 'RUN_INFO') && class_index == 1
+                        provider.RUN_INFO_CLASS = new_class;
+                    end
+                end
+            end
+        end
+        
+        %parallel runs
+        function provider = update_parameter_file_yaml(provider, param_file_number)
+            %change parameter file
+            parameter_file = [provider.PARA.result_path provider.PARA.run_name '/' provider.PARA.run_name '_' num2str(param_file_number) '.yml'];
+            provider.PARA.parameter_file = parameter_file;
+        end
+        
+        
+        function provider = update_run_name_yaml(provider, worker_number)
+            %change run name and make new result directories if necessary
+            provider.PARA.run_name = [provider.PARA.run_name '_' num2str(worker_number)];
+            if ~(exist([provider.PARA.result_path provider.PARA.run_name ])==7)
+                mkdir([provider.PARA.result_path provider.PARA.run_name]);
+            end
+        end
+        
+    end
+end
+
