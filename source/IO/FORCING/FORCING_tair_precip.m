d2aed04122cc810c50fe7e0b390ebc67a903208e
Thomas Ingeman-Nielsen <thomas@i-n.dk>
2021-12-07 09:03:52 +0100

    Bugfixes and comments in some FORCING classes.

--- source/IO/FORCING/FORCING_tair_precip.m
+++ source/IO/FORCING/FORCING_tair_precip.m
@@ -86,15 +86,14 @@ classdef FORCING_tair_precip < matlab.mixin.Copyable
             if isempty(forcing.PARA.start_time) || ~ischar(forcing.PARA.start_time)
                 forcing.PARA.start_time = forcing.DATA.timeForcing(1,1);
             else
-                forcing.PARA.start_time = datenum(forcing.PARA.start_time, 'dd.mm.yyyy');
+                forcing.PARA.start_time = datenum(forcing.PARA.start_time(1,1), forcing.PARA.start_time(2,1), forcing.PARA.start_time(3,1));
             end
             
             % handle end time, if specified
             if isempty(forcing.PARA.end_time) || isnan(forcing.PARA.end_time(1,1))
                 forcing.PARA.end_time = floor(forcing.DATA.timeForcing(end,1));
             else
-                forcing.PARA.end_time = datenum(forcing.PARA.end_time, 'dd.mm.yyyy');
-				forcing.PARA.end_time = datenum(forcing.PARA.end_time(1,1), forcing.PARA.end_time(2,1),forcing.PARA.end_time(3,1));
+                forcing.PARA.end_time = datenum(forcing.PARA.end_time(1,1), forcing.PARA.end_time(2,1),forcing.PARA.end_time(3,1));
             end
             
             %initialize TEMP
