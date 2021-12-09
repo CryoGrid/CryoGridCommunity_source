c2574773f5a694de57d8f4a6f2d7cbc229149f08
Thomas Ingeman-Nielsen <thomas@i-n.dk>
2021-12-09 22:37:24 +0100

    Added new RUN_3D_PARALLEL class to allow running tiles in parallel using a parfor loop

---
+++ source/IO/RUN_INFO/RUN_3D_PARALLEL.m
@@ -0,0 +1,155 @@
+% base class build a model tile
+
+classdef RUN_3D_PARALLEL < matlab.mixin.Copyable
+    
+    properties
+        PPROVIDER
+        PARA
+        CONST
+        STATVAR
+        TILE
+        %pprovider
+    end
+    
+    
+    methods
+        
+        
+        function run_info = provide_PARA(run_info)
+            
+            run_info.PARA.number_of_cores = [];
+            run_info.PARA.number_of_tiles = []; %3;
+            run_info.PARA.param_file_number = []; %[1;2;3];
+
+            run_info.PARA.connected = [];
+            run_info.PARA.contact_length = [];
+            run_info.PARA.distance = [];
+
+            run_info.PARA.tile_class = [];
+            run_info.PARA.tile_class_index = [];
+
+        end
+        
+        function run_info = provide_CONST(run_info)
+
+        end
+        
+        function run_info = provide_STATVAR(run_info)
+
+        end
+        
+%         function run_info = initialize_excel(run_info)
+%             
+%         end
+        
+        function run_info = finalize_init(run_info)
+ 
+        end
+        
+        
+        
+        function [run_info, tile] = run_model(run_info)
+            %this could first open spmd and assign run_number depending on
+            %worker, then do another round of pprovider
+            %it could also do a loop over different tile representing
+            %different sections of the run, e.g. initial inial init, spin-up, actual run 
+
+            % The run_info and tile instances returned are not the ones
+            % actually used in the runs, it is the template instances
+            % that were passed to the function.
+            
+            %for tile_id = 1:run_info.PARA.number_of_tiles
+            %    exceptions.(['TILE_', num2str(tile_id)]) = struct();
+            %end
+
+            err_out = cell(run_info.PARA.number_of_tiles);
+
+            parpool([1 run_info.PARA.number_of_cores])
+            parfor tile_id = 1:run_info.PARA.number_of_tiles
+                % make copy of template run_info, to modify in this process
+                this_run_info = copy(run_info);
+
+                this_run_info.PARA.worker_number = tile_id; % assign id
+                
+                % prepare error collection in case of exceptions
+                err_out{tile_id}.tile_id = tile_id;
+                err_out{tile_id}.OK = true;
+
+                try
+                    % initialize and run tile instance
+                    [out_run_info, out_tile] = kernel_run_model(this_run_info);
+                catch ME
+                    % catch and store error for saving after pool completes
+                    error_timestamp = now;
+
+                    err_out{tile_id}.tile_id = tile_id;
+                    err_out{tile_id}.OK = false;
+                    err_out{tile_id}.run_info = this_run_info;
+                    err_out{tile_id}.timestamp = error_timestamp;
+                    err_out{tile_id}.MException = ME;
+
+                    % we cannot use save inside parfor, so we have to store
+                    % the information and save it after.
+                end
+            end
+
+            % now save any error logs
+            for tid = 1:length(run_info.PARA.number_of_tiles)
+                if ~err_out{tid}.OK
+                    error_log_file = [err_out{tid}.run_info.PPROVIDER.PARA.result_path, ...
+                                      'errorlog_', ...
+                                      datestr(err_out{tid}.timestamp, 'yyyymmdd_HHMMSS'), '__', ...
+                                      err_out{tid}.run_info.PPROVIDER.PARA.run_name, ...
+                                      '__tile_', num2str(err_out{tid}.tile_id), ...
+                                      '.mat'];
+    
+                    save(error_log_file, '-struct', err_out{tid});
+                end
+            end
+
+            tile = run_info.TILE;
+        end
+ 
+
+        function [run_info, tile] = kernel_run_model(run_info)
+            % This is the actual normal run_model method. It is extracted
+            % in separate method to more easily enclose its execution in
+            % a try-catch block in the new run_model method.
+
+            %update the worker-specific name of the parameter file and the run
+            run_info.PPROVIDER = update_parameter_file(run_info.PPROVIDER, run_info.PARA.param_file_number(run_info.PARA.worker_number,1));
+            run_info.PPROVIDER = update_run_name(run_info.PPROVIDER, run_info.PARA.worker_number);
+                            
+            %read worker-specific parameter file
+            run_info.PPROVIDER = read_parameters(run_info.PPROVIDER);
+                            
+            run_info = customize(run_info);
+            
+            %tile = run_info.PPROVIDER.FUNCTIONAL_CLASSES.TILE{run_info.PARA.tile_number,1};
+            tile = copy(run_info.PPROVIDER.CLASSES.(run_info.PARA.tile_class){run_info.PARA.tile_class_index,1});
+
+            tile.RUN_INFO = run_info;
+            run_info.TILE = tile;
+
+            tile = finalize_init(tile);
+            
+            tile = run_model(tile);  %time integration
+        end
+    
+
+        function run_info = customize(run_info)
+            %FUNCTION TO BE EDITED BY USER - when inheriting from this class
+            %here customizations can be done by directly writing pprovider
+            %one can for example change parameters in the different
+            %subsurface classes
+            %run_info.PPROVIDER.FUNCTIONAL_CLASSES.XX = YY;
+            %
+            %use run_info.PARA.worker_number for customization
+
+        end
+        
+    end
+end
+
+
+
